8dd71776146e8ca262b778b01cb5101f5ec58c25
OxOr <joangamers12@gmail.com>
2022-04-12 07:58:34 -0500

    Arreglo de interfaz - menu

--- app/src/main/java/com/wikitude/wikitudestudioandroidapptemplate/MainActivity.java
+++ app/src/main/java/com/wikitude/wikitudestudioandroidapptemplate/MainActivity.java
@@ -1,13 +1,30 @@
 package com.wikitude.wikitudestudioandroidapptemplate;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView cardInicio;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //OCULTAR ACTION BAR
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.hide();
+
+        cardInicio = findViewById(R.id.cardINICIO);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
--- app/src/main/java/com/wikitude/wikitudestudioandroidapptemplate/Splash.java
+++ app/src/main/java/com/wikitude/wikitudestudioandroidapptemplate/Splash.java
@@ -3,18 +3,14 @@ package com.wikitude.wikitudestudioandroidapptemplate;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.ActionBarOverlayLayout;
+
 
 public class Splash extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //OCULTAR ACTION BAR
-        //ActionBar actionbar = getSupportActionBar();
-        //actionbar.hide();
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
