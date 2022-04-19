661f16853ec51c85ae17901463c188ce00f56ae3
OxOr <joangamers12@gmail.com>
2022-04-18 22:29:20 -0500

    TERMINADO

--- app/src/main/java/com/wikitude/wikitudestudioandroidapptemplate/ContactosFragment.java
+++ app/src/main/java/com/wikitude/wikitudestudioandroidapptemplate/ContactosFragment.java
@@ -1,24 +1,35 @@
 package com.wikitude.wikitudestudioandroidapptemplate;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.ViewUtils;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ContactosFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ContactosFragment extends Fragment {
+//IMPLEMENTAMOS EL ONCLICKLISTENER
 
+public class ContactosFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_contacts, container, false);
     }
+
+
 }
